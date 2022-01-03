dda55f6ae38045cb321d42ccbccd096986c870d9
Venediktov <venediktov.vladimir.s@gmail.com>
2022-01-04 00:00:00 +0300

    Стирание типов

--- src/main/java/ru/venediktov/javacore/generics/TypeErasure.java
+++ src/main/java/ru/venediktov/javacore/generics/TypeErasure.java
@@ -4,33 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Информация о типе параметра в generic стирается после компиляции
+ * Информация о типе параметра T в generic стирается после компиляции.
  */
 public class TypeErasure<T> {
 
   private T value1;
+  private T value2;
 
-  public void printValues() {
-    System.out.println(value1);
+  public void printValues(TypeErasure<T> typeErasure) {
+    System.out.println(String.format("%s and %s", value1, value2));
   }
 
-  public static <T> TypeErasure<T> createAndAddValue(Object o1) {
+  public static <T> TypeErasure<T> createAndAddValues(Object o1, Object o2) {
     TypeErasure<T> result = new TypeErasure<>();
-    result.value1 = (T) o1;
+    result.value1 = (T) o1;// тут никакой ошибки не будет, т.к кастить будет в Object (из-за стирания типа), а не в Integer
+    result.value2 = (T) o2;
     return result;
   }
 
   public static void main(String[] args) {
     // example 1
     Double d = 22.111;
-    TypeErasure<Integer> test = createAndAddValue(d); // во время компиляции это стало TypeErasure<Object> test
-    test.printValues();
+    String s = "String!";
+    TypeErasure<Integer> test = createAndAddValues(d, s); // во время компиляции это стало TypeErasure<Object> test
+    try {
+      int intValue = test.value1.intValue();
+    } catch (Exception e) {
+      System.err.println(e);
+    }
+    test.printValues(test);
 
     // example 2
     List<String> strings = new ArrayList<>();
     List<Integer> numbers = new ArrayList<>();
 
-    System.out.println(strings.getClass());
     System.out.println(strings.getClass() == numbers.getClass()); // true
   }
 
