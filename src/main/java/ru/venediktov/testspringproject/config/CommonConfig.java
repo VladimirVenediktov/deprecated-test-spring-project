7130a7e3565861d079f49c37422b81b11be419d0
Venediktov <venediktov.vladimir.s@gmail.com>
2022-01-10 01:59:19 +0300

    подготовка для настройки вывода метрик через prometheus + grafana

--- src/main/java/ru/venediktov/testspringproject/config/CommonConfig.java
+++ src/main/java/ru/venediktov/testspringproject/config/CommonConfig.java
@@ -1,5 +1,8 @@
 package ru.venediktov.testspringproject.config;
 
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,14 @@ public class CommonConfig {
     ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
     messageSource.setBasename("messages");
     return messageSource;
+  }
+
+  // нужен для решения проблемы пропадания некоторых метрик, отдаваемых в Prometheus, после включения в проект
+  // spring-boot-starter-data-jpa
+  @Bean
+  InitializingBean forcePrometheusPostProcessor(BeanPostProcessor meterRegistryPostProcessor,
+      PrometheusMeterRegistry registry) {
+    return () -> meterRegistryPostProcessor.postProcessAfterInitialization(registry, "");
   }
 
 }
