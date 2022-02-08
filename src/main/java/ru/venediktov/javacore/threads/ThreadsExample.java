3baf486a589233f05fe28f21e0ab790d94546339
Venediktov <venediktov.vladimir.s@gmail.com>
2022-02-08 23:07:36 +0300

    функц-й интерфейс и лямбда, простейший поток

--- src/main/java/ru/venediktov/javacore/threads/ThreadsExample.java
+++ src/main/java/ru/venediktov/javacore/threads/ThreadsExample.java
@@ -9,9 +9,19 @@ public class ThreadsExample {
     // простейшее создание отдельного потока
     Runnable task = () -> System.out.println("New thread is created!");
     new Thread(task).start();
+    // или
+    new NewThread().start();
 
     //new RunnableTaskExample().execute();
     //new CallableTaskExample().execute();
     //new SynchronizedExample().execute();
   }
+
+  public static class NewThread extends Thread {
+    @Override
+    public void run() {
+      System.out.println("Hello from your new thread!");
+    }
+  }
+
 }
