00a45587bf9af21ef714a3745715615671b0d817
Venediktov <venediktov.vladimir.s@gmail.com>
2022-02-10 01:26:01 +0300

    ArrayList vs LinkedList

--- src/main/java/ru/venediktov/javacore/datastructure/Demo.java
+++ src/main/java/ru/venediktov/javacore/datastructure/Demo.java
@@ -9,6 +9,21 @@ import java.util.ListIterator;
 import java.util.Random;
 import lombok.SneakyThrows;
 
+/**
+ * Операция                 Метод               ArrayList         LinkedList
+ * ----------------------------------------------------------------------------
+ * Добавление             add(value)            быстро *        очень быстро
+ * Вставка                add(index, value)     медленно        очень медленно **
+ * Вставка ч/з итератор   it.add(value)         медленно        очень быстро
+ * Получение элемента     get(index)            очень быстро    очень медленно **
+ * Изменение элемента     set(index, value)     очень быстро    очень медленно **
+ * Удаление элемента      remove(index)         медленно        очень медленно **
+ * Удаление ч/з итератор  it.remove(index)      медленно        очень быстро
+ *
+ * (*) - чуть медленнее, если нужно увеличивать capacity
+ * (**) - последовательный перебор элементов связного списка - очень медленный
+ *        искл-е: итератор или вставка/удаление в начале списка
+ */
 public class Demo {
 
   @SneakyThrows
@@ -52,7 +67,7 @@ public class Demo {
     while (iterator.hasNext()) {
       iterator.next();
       iterator.set(new Random().nextInt());
-      //linkedList.add(4);//нельзя так, ConcurrentModificationException
+      //linkedList.add(4);//нельзя так, ConcurrentModificationException (в цикле for-each тоже)
     }
     long end = System.currentTimeMillis();
     System.out.println(String.format("Элементы добавлены за %d мс", end - start));
